20bea732e311a407f7c93c21f7a467efa72127b0
craftteam <teamcodecraft70.com>
2024-12-10 17:38:30 +0400

    ftechy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved-2024</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="#" target={"blank"}>
+            Youtube
+          </a>
+          <a href="#" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/noorul-ain" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
